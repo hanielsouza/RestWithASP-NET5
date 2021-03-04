90ed9e8b9a9a3b299c7c1a983f9cde70a7441acc
hanielsouza <hanielsouza@hotmail.com>
2021-03-04 15:36:19 -0300

    working with four verbs

---
+++ 01_RestWithASPNET5_ScaffoldViaVS/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        
+        private readonly ILogger<PersonController> _logger;
+        
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secoundNumber}")]
+        public IActionResult Sum(string firstNumber,string secoundNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secoundNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber)  + ConvertToDecimal(secoundNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number) ;
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber,out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        
+    }
+}
---
+++ 01_RestWithASPNET5_ScaffoldViaVS/RestWithASPNET5Udemy/RestWithASPNET5Udemy/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
